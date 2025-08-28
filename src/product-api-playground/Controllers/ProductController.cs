8a80d7614777771a974ce4bb1f6073595b4ed112
Alam Alfaris <alamtahfizer@gmail.com>
2025-08-28 22:38:43 +0700

    create controller

---
+++ src/product-api-playground/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace product_api_playground.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult HelloWorldAsync()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
